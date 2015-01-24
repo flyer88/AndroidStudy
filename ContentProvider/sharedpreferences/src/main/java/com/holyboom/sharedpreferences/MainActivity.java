78848490fe0a5d36e251589c7b6cb0714bea7881
flyer <253311459@qq.com>
2015-01-24 17:01:29 +0800

    sharedpreferences读取

--- ContentProvider/sharedpreferences/src/main/java/com/holyboom/sharedpreferences/MainActivity.java
+++ ContentProvider/sharedpreferences/src/main/java/com/holyboom/sharedpreferences/MainActivity.java
@@ -3,6 +3,7 @@ package com.holyboom.sharedpreferences;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,13 +12,14 @@ import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
-     Button saveData;
-
+    Button saveData;
+    Button restoreData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         saveData = (Button)findViewById(R.id.save_data);
+        restoreData = (Button)findViewById(R.id.restore_data);
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,6 +28,19 @@ public class MainActivity extends ActionBarActivity {
                 editor.putInt("age",28);
                 editor.putBoolean("married",false);
                 editor.commit();
+            }
+        });
+
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref= getSharedPreferences("data",MODE_PRIVATE);
+                String name = pref.getString("name","");
+                int age =pref.getInt("age",0);
+                boolean married = pref.getBoolean("married",false);
+                Log.e("sharedpreferences","name is "+name);
+                Log.e("sharedpreferences","age is "+age);
+                Log.e("sharedpreferences","married is "+married);
             }
         });
     }
