a64cf1a06855620cf3b5353d983d902f44491a55
flyer <253311459@qq.com>
2015-01-24 16:41:59 +0800

    完成文件操作

--- ContentProvider/app/src/main/java/com/holyboom/contentprovider/MainActivity.java
+++ ContentProvider/app/src/main/java/com/holyboom/contentprovider/MainActivity.java
@@ -3,13 +3,15 @@ package com.holyboom.contentprovider;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 
@@ -22,6 +24,12 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         contentEdit = (EditText)findViewById(R.id.content);
+        String inputText = load();
+        if (!TextUtils.isEmpty(inputText)){
+            contentEdit.setText(inputText);
+            contentEdit.setSelection(inputText.length());
+            Toast.makeText(MainActivity.this,"restoring succeed",Toast.LENGTH_SHORT).show();
+        }
 
     }
 
@@ -50,5 +58,30 @@ public class MainActivity extends ActionBarActivity {
                 e.printStackTrace();
             }
         }
+    }
+
+    public String load(){
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine())!=null){
+                content.append(line);
+            }
+        }catch (Exception e){
+
+        }finally {
+            if (reader!=null){
+                try {
+                    reader.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
     }
 }
