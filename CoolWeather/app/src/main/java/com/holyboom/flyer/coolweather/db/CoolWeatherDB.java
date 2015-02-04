4d6afcbb0e0bec85dce826cda6f64323bebdbbf8
flyer88 <253311459@qq.com>
2015-02-04 13:14:34 +0800

    add CollWeatherDB

--- CoolWeather/app/src/main/java/com/holyboom/flyer/coolweather/db/CoolWeatherDB.java
+++ CoolWeather/app/src/main/java/com/holyboom/flyer/coolweather/db/CoolWeatherDB.java
@@ -2,9 +2,15 @@ package com.holyboom.flyer.coolweather.db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.holyboom.flyer.coolweather.model.City;
+import com.holyboom.flyer.coolweather.model.County;
 import com.holyboom.flyer.coolweather.model.Province;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by flyer on 15/2/3.
@@ -42,10 +48,94 @@ public class CoolWeatherDB {
         return coolWeatherDB;
     }
 
+    /**
+     * 将Province实例存放入数据库
+     * @param province
+     */
     public void saveProvince(Province province){
         if (province != null){
             ContentValues values = new ContentValues();
+            values.put("province_name",province.getProvinceName());
+            values.put("Province_code",province.getProvinceCode());
+            db.insert("Province",null,values);
         }
     }
 
+    /**
+     * 将City实例存放入数据库
+     * @param city
+     */
+    public void saveCity(City city){
+        if (city != null){
+            ContentValues values = new ContentValues();
+            values.put("city_name",city.getCityName());
+            values.put("city_code",city.getCityCode());
+            db.insert("City",null,values);
+        }
+    }
+
+    /**
+     * 将County实例存放入数据库
+     * @param county
+     */
+    public void savecounty(County county){
+        if (county != null){
+            ContentValues values = new ContentValues();
+            values.put("county_name",county.getCountyName());
+            values.put("county_code",county.getCountyCode());
+            db.insert("county",null,values);
+        }
+    }
+
+    /**
+     * 将所有省的信息从数据库中读取出来
+     * @return
+     */
+    public List<Province> loadProvinces(){
+        List<Province> list = new ArrayList<Province>();
+        Cursor cursor = db.query("Province",null,null,null,null,null,null);
+        if (cursor.moveToFirst()){
+            do {
+                Province province = new Province();
+                province.setId(cursor.getInt(cursor.getColumnIndex("id")));
+                province.setProvinceName(cursor.getString(cursor.getColumnIndex("province_name")));
+                province.setProvinceCode(cursor.getString(cursor.getColumnIndex("province_code")));
+                list.add(province);
+            }while (cursor.moveToNext());
+        }
+        return list;
+    }
+
+    /**
+     * 将所有城市的信息从数据库中读取出来
+     * @param provinceId
+     * @return
+     */
+    public List<City> loadCities(int provinceId){
+        List<City> list = new ArrayList<City>();
+        Cursor cursor = db.query("City",null,"province_id = ?",new String[]{String.valueOf(provinceId)},null,null,null);
+        do {
+            City city = new City();
+            city.setId(cursor.getInt(cursor.getColumnIndex("id")));
+            city.setCityCode(cursor.getString(cursor.getColumnIndex("city_code")));
+            city.setCityName(cursor.getString(cursor.getColumnIndex("city_name")));
+            city.setProvinceId(provinceId);
+            list.add(city);
+        }while (cursor.moveToNext());
+        return list;
+    }
+
+    public List<County> loadCounties(int cityId){
+        List<County> list = new ArrayList<County>();
+        Cursor cursor = db.query("County",null,"city_id = ?",new String[]{String.valueOf(cityId)},null,null,null);
+        do {
+            County county = new County();
+            county.setId(cursor.getInt(cursor.getColumnIndex("id")));
+            county.setCountyCode(cursor.getString(cursor.getColumnIndex("county_code")));
+            county.setCountyName(cursor.getString(cursor.getColumnIndex("county_name")));
+            county.setCityId(cityId);
+            list.add(county);
+        }while (cursor.moveToNext());
+        return list;
+    }
 }
