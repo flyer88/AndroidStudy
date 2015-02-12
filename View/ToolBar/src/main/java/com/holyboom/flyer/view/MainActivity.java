92137521706d2c9fd48840da4e579cec632e2551
flyer88 <253311459@qq.com>
2015-02-12 17:33:51 +0800

    add viewpager

--- View/ToolBar/src/main/java/com/holyboom/flyer/view/MainActivity.java
+++ View/ToolBar/src/main/java/com/holyboom/flyer/view/MainActivity.java
@@ -1,18 +1,27 @@
 package com.holyboom.flyer.view;
 
 //import android.app.Activity;
-import android.graphics.Color;
+
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.Menu;
-import android.view.MenuItem;
-//import android.widget.Toolbar;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
+
+//import android.widget.Toolbar;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -21,44 +30,86 @@ public class MainActivity extends ActionBarActivity {
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     ListView listView;
-    ActionBarDrawerToggle actionBarDrawerToggle;
+        ActionBarDrawerToggle actionBarDrawerToggle;
 
     String[] strings = {"A","B","C","D"};
     ArrayAdapter arrayAdapter;
+
+    ViewPager viewPager;
+    List<View> viewPagerList = new ArrayList<View>() ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
+        initToolbar();
+        initDrawerLayout();
+        initViewPager();
 
+    }
+    public void initToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_navigation);
+        listView = (ListView) findViewById(R.id.list_view);
         toolbar.setTitle("Toolbar");//设置Toolbar标题
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //setActionBar(toolbar);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout,toolbar,R.string.app_name,R.string.app_name){
+
+    }
+    public void initDrawerLayout(){
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, toolbar, R.string.app_name, R.string.app_name) {
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
 
             }
+
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
 
             }
         };
-
-
         actionBarDrawerToggle.syncState();
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, strings);
         listView.setAdapter(arrayAdapter);
 
     }
-    public void initView(){
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_navigation);
-        listView = (ListView) findViewById(R.id.list_view);
+    public void initViewPager(){
+        Log.e("initViewPager","初始化viewpager");
+
+        LayoutInflater layoutInflater = getLayoutInflater().from(this);
+        viewPagerList.add(layoutInflater.inflate(R.layout.item1,null));
+        viewPagerList.add(layoutInflater.inflate(R.layout.item2,null));
+        viewPagerList.add(layoutInflater.inflate(R.layout.item3,null));
+        viewPager = (ViewPager)findViewById(R.id.view_pager);
+        viewPager.setAdapter(new PagerAdapter() {
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                container.addView(viewPagerList.get(position));
+                Log.e("instantiateItem viewpager","position is "+position);
+                return viewPagerList.get(position);
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView(viewPagerList.get(position));
+                Log.e("destroyItem viewpager","position is "+position);
+            }
+
+            @Override
+            public int getCount() {
+                return viewPagerList.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+        });
+
+
     }
 }
