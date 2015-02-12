3801963e0142b05839d5043dbc35f2f943420bc6
flyer88 <253311459@qq.com>
2015-02-12 14:23:00 +0800

    drawer navigation

--- View/ToolBar/src/main/java/com/holyboom/flyer/view/MainActivity.java
+++ View/ToolBar/src/main/java/com/holyboom/flyer/view/MainActivity.java
@@ -1,55 +1,64 @@
 package com.holyboom.flyer.view;
 
-import android.os.Bundle;
+//import android.app.Activity;
+import android.graphics.Color;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.ShareActionProvider;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
+//import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends ActionBarActivity {
 
 
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    ListView listView;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    String[] strings = {"A","B","C","D"};
+    ArrayAdapter arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
 
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-
-        toolbar.setTitle("toolbar");
-
+        toolbar.setTitle("Toolbar");//设置Toolbar标题
         setSupportActionBar(toolbar);
-
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //setActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout,toolbar,R.string.app_name,R.string.app_name){
             @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.action_settings:
-                        Toast.makeText(MainActivity.this,"action_settings",Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.action_share:
-                        Toast.makeText(MainActivity.this,"action_share",Toast.LENGTH_SHORT).show();
-                        break;
-                    default:
-                        break;
-                }
-                return true;
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
             }
-        });
-    }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            }
+        };
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+
+        actionBarDrawerToggle.syncState();
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, strings);
+        listView.setAdapter(arrayAdapter);
+
+    }
+    public void initView(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_navigation);
+        listView = (ListView) findViewById(R.id.list_view);
     }
 }
