f6ec8431debbd08ac390c09d75f4bb3f4bd13b02
flyer88 <253311459@qq.com>
2015-02-14 17:36:21 +0800

    最小限定符（qualifiers）

--- Fragment/app/src/main/java/com/holyboom/flyer/fragment/MainActivity.java
+++ Fragment/app/src/main/java/com/holyboom/flyer/fragment/MainActivity.java
@@ -8,29 +8,29 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(this);
+      //  button.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.button:
-                AnotherRightFragment  fragment = new AnotherRightFragment();
-                FragmentManager fragmentManager = getFragmentManager();
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
-                transaction.replace(R.id.right_layout,fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
-                break;
-            default:
-                break;
-        }
-    }
+//   @Override
+//    public void onClick(View v) {
+//        switch (v.getId()){
+//            case R.id.button:
+//                AnotherRightFragment  fragment = new AnotherRightFragment();
+//                FragmentManager fragmentManager = getFragmentManager();
+//                FragmentTransaction transaction = fragmentManager.beginTransaction();
+//                transaction.replace(R.id.right_layout,fragment);
+//                transaction.addToBackStack(null);
+//                transaction.commit();
+//                break;
+//            default:
+//                break;
+//        }
+//    }
 }
